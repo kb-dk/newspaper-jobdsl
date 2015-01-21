1866c297c1a72aa807498b40715c021f1273dd75
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2015-01-21 12:47:15 +0100

    Updated batch event framework

--- buildJobs/newspaper-batch-event-framework.groovy
+++ buildJobs/newspaper-batch-event-framework.groovy
@@ -16,6 +16,32 @@ job(type: Maven) {
     }
     jdk('java7')
 
+    preBuildSteps {
+        shell("echo \"Getting the integration test config from Stash\"\n" +
+                "cd \"$WORKSPACE\"\n" +
+                "mkdir -p \"build\"\n" +
+                "cd build\n" +
+                "if [ -d \"devel-config\" ]; then\n" +
+                "  cd devel-config\n" +
+                "  git pull\n" +
+                "else\n" +
+                "  git clone ssh://git@sbprojects.statsbiblioteket.dk:7999/avis/devel-config.git\n" +
+                "fi")
+        shell("echo \"Getting the sample batch from Stash\"\n" +
+                "cd \"$WORKSPACE\"\n" +
+                "mkdir -p \"build\"\n" +
+                "cd build\n" +
+                "if [ -d \"testdata\" ]; then\n" +
+                "  cd testdata\n" +
+                "  git pull\n" +
+                "else\n" +
+                "  git clone ssh://git@sbprojects.statsbiblioteket.dk:7999/avis/testdata.git\n" +
+                "fi")
+
+    }
+    postBuildSteps {
+        shell("echo 'run after Maven'")
+    }
     mavenInstallation('maven3')
     mavenOpts('-Pnewspaper')
     mavenOpts('-PintegrationTests')
