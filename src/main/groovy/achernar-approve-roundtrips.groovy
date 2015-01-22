bb57fd697ea5d609d4f3d00936cef22200eea53c
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2015-01-22 12:56:34 +0100

    stub approve roundtrip

--- src/main/groovy/achernar-approve-roundtrips.groovy
+++ src/main/groovy/achernar-approve-roundtrips.groovy
@@ -1,1 +1,10 @@
+import javaposse.jobdsl.dsl.Job
+import javaposse.jobdsl.dsl.JobType
+
+Job job = job(type: JobType.Freeform) {}
+NewspaperUtilities.addAchernar(job)
+
+job.with {
+    name 'achernar-approve-roundtrips'
+    }
 //TODO
