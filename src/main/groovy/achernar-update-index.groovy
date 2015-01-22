31c69a142e2f1a77c0745cc6e760a6e9e57c95d1
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2015-01-22 13:56:59 +0100

    Other format for triggers

--- src/main/groovy/achernar-update-index.groovy
+++ src/main/groovy/achernar-update-index.groovy
@@ -15,40 +15,21 @@ job.with {
         shell('set +e\n' +
                 'set +x\n' +
                 'ssh doms@achernar "source .bash_profile ; shopt -s huponexit; ~/7880-cibuild/sboi-summarise/bin/ingest_update.sh  && ~/7880-cibuild/sboi-summarise/bin/index_update.sh < /dev/null &> /dev/null"')
-
-    }
-    postBuildSteps{
         downstreamParameterized {
-            trigger('achernar-invoke-batch-trigger', 'SUCCESS',true){
-            }
-            trigger('achernar-ingest-metadata', 'SUCCESS',true) {
-            }
-            trigger('achernar-ingest-data', 'SUCCESS',true) {
-            }
-            trigger('achernar-check-structure', 'SUCCESS') {
-            }
-            trigger('achernar-generate-statistics', 'SUCCESS') {
-            }
-            trigger('achernar-check-metadata', 'SUCCESS') {
-            }
-            trigger('achernar-generate-histogram', 'SUCCESS') {
-            }
-            trigger('achernar-generate-jpylyzer', 'SUCCESS') {
-            }
-            trigger('achernar-generate-presentation', 'SUCCESS') {
-            }
-            trigger('achernar-enrich-batch', 'SUCCESS') {
-            }
-            trigger('achernar-generate-manualQA-flags', 'SUCCESS') {
-            }
-            trigger('achernar-approve-roundtrip', 'SUCCESS') {
-            }
-            trigger('achernar-update-edition-records', 'SUCCESS') {
-            }
-            trigger('achernar-update-title-records', 'SUCCESS') {
-            }
-
-
+            trigger('achernar-invoke-batch-trigger', 'SUCCESS',true)
+            trigger('achernar-ingest-metadata', 'SUCCESS',true)
+            trigger('achernar-ingest-data', 'SUCCESS',true)
+            trigger('achernar-check-structure', 'SUCCESS')
+            trigger('achernar-generate-statistics', 'SUCCESS')
+            trigger('achernar-check-metadata', 'SUCCESS')
+            trigger('achernar-generate-histogram', 'SUCCESS')
+            trigger('achernar-generate-jpylyzer', 'SUCCESS')
+            trigger('achernar-generate-presentation', 'SUCCESS')
+            trigger('achernar-enrich-batch', 'SUCCESS')
+            trigger('achernar-generate-manualQA-flags', 'SUCCESS')
+            trigger('achernar-approve-roundtrip', 'SUCCESS')
+            trigger('achernar-update-edition-records', 'SUCCESS')
+            trigger('achernar-update-title-records', 'SUCCESS')
         }
     }
 }
